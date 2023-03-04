4e7cd0e687cdfaddc06170154fdb5dadb7bd225f
Idanbenaim <idanbenaim@gmail.com>
2023-03-04 16:01:44 +0200

    images working

--- base/models.py
+++ base/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Students(models.Model):
    name = models.CharField(max_length=50,null=True,blank=True)
    age = models.IntegerField()
-   image = models.ImageField(null=True,blank=True,default='/placeholder.png')
+   image = models.ImageField(null=True,blank=True,default='/placeholder.png',upload_to='Posted_Images')
 
    def __str__(self):
           return self.name
